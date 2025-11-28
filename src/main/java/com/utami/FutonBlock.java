92683d5556706d7b9c2a762820c466d315ca6430
futonid <cpherid@protonmail.com>
2025-11-28 16:05:21 +0300

    fix: futon drop behavior + added 5 new languages

--- src/main/java/com/utami/FutonBlock.java
+++ src/main/java/com/utami/FutonBlock.java
@@ -1,10 +1,15 @@
 package com.utami;
 
 import net.minecraft.block.*;
+import net.minecraft.block.enums.BedPart;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.context.LootContextParameterSet;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.util.math.BlockPos;
+
+import java.util.List;
 
 
 public class FutonBlock extends BedBlock {
@@ -25,4 +30,14 @@ public class FutonBlock extends BedBlock {
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
     }
+    //quick fix for drops
+    @Override
+    public List<ItemStack> getDroppedStacks(BlockState state, LootContextParameterSet.Builder builder) {
+        // Only Head Drop
+        if (state.get(PART) == BedPart.FOOT) {
+            return List.of();
+        }
+        return List.of(new ItemStack(this.asItem()));
+    }
 }
+
